87205e530eb99ae73a6c45d13545f4fa29bd4ccc
Moran3277 <40189178@continental.edu.pe>
2025-04-22 00:55:44 -0500

    Update routes.py - Rutas para obtener los posts en formato JSON:
    define el endpoint /posts

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,1 +1,10 @@
+from flask import Blueprint, jsonify
+from .models import Post
+
+routes = Blueprint('routes', __name__)
+
+@routes.route('/posts', methods=['GET'])
+def get_posts():
+    posts = Post.query.all()
+    return jsonify([post.to_dict() for post in posts])
 
